06a55d76e1ad068b7808248031cf16dc9259b48e
C Chaitanya <chaitu9701@gmail.com>
2020-08-07 17:11:56 +0530

    Update dataquest_crawler.py

--- dataquest_crawler.py
+++ dataquest_crawler.py
@@ -1,5 +1,5 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-r = requests.get('https://app.dataquest.io/login', auth=HTTPBasicAuth("chaitu9701@gmail.com", "Donkey09"))
+r = requests.get('https://app.dataquest.io/login', auth=HTTPBasicAuth("", ""))
 resp = json.loads(r.content)
